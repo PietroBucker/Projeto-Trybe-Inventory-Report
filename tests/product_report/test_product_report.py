6fea1e6c9baeb9a470aa21f16c0720c42f9a426c
Pietro Bucker <pietro_177@hotmail.com>
2023-08-25 15:24:24 -0300

    requisito 2 completo

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
+expect = (
+    "The product 1 - FIFA with serial number 123456 manufactured on "
+    "01/01/2020 by the company EA valid until 01/01/2021 must be "
+    "stored according to the following instructions: jogo para familia."
+)
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    produto = Product(
+        "1",
+        "FIFA",
+        "EA",
+        "01/01/2020",
+        "01/01/2021",
+        "123456",
+        "jogo para familia",
+    )
+    assert produto.__str__() == expect
