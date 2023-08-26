bd6fe606112d17ba19d02f3b71141116dfaab9b0
Pietro Bucker <pietro_177@hotmail.com>
2023-08-25 21:53:00 -0300

    requisito 7 test2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -14,13 +14,14 @@ class SimpleReport(Report):
     def generate(self) -> str:
         larges_inv: dict[str, int] = dict()
         for stock in self.stock:
-            oldest_date = SimpleReport.old_manufacturing(stock.data)
-            closest_date = SimpleReport.close_expiration(stock.data)
-            for el in stock.data:
-                if el.company_name not in larges_inv:
-                    larges_inv[el.company_name] = 1
-                else:
-                    larges_inv[el.company_name] += 1
+            if stock.data:
+                oldest_date = SimpleReport.old_manufacturing(stock.data)
+                closest_date = SimpleReport.close_expiration(stock.data)
+                for el in stock.data:
+                    if el.company_name not in larges_inv:
+                        larges_inv[el.company_name] = 1
+                    else:
+                        larges_inv[el.company_name] += 1
         return (
             f"Oldest manufacturing date: {oldest_date} "
             f"Closest expiration date: {closest_date} "
@@ -54,52 +55,53 @@ class SimpleReport(Report):
         )
 
 
-# produto = Product(
-#     id="1",
-#     product_name="Nicotine Polacrilex",
-#     company_name="Target Corporation",
-#     manufacturing_date="2020-02-18",
-#     expiration_date="2023-09-26",
-#     serial_number="CR25 1551 4467 2549 4402 1",
-#     storage_instructions="instrucao 1",
-# )
+produto = Product(
+    id="1",
+    product_name="Nicotine Polacrilex",
+    company_name="Target Corporation",
+    manufacturing_date="2020-02-18",
+    expiration_date="2023-09-26",
+    serial_number="CR25 1551 4467 2549 4402 1",
+    storage_instructions="instrucao 1",
+)
 
-# produto2 = Product(
-#     id="1",
-#     product_name="Nicotine Polacrilex",
-#     company_name="Corporation",
-#     manufacturing_date="2022-01-18",
-#     expiration_date="2023-09-17",
-#     serial_number="CR25 1551 4467 2549 4402 1",
-#     storage_instructions="instrucao 1",
-# )
+produto2 = Product(
+    id="1",
+    product_name="Nicotine Polacrilex",
+    company_name="Corporation",
+    manufacturing_date="2022-01-18",
+    expiration_date="2023-09-17",
+    serial_number="CR25 1551 4467 2549 4402 1",
+    storage_instructions="instrucao 1",
+)
 
-# produto3 = Product(
-#     id="1",
-#     product_name="Nicotine Polacrilex",
-#     company_name="Target",
-#     manufacturing_date="2021-02-18",
-#     expiration_date="2023-08-27",
-#     serial_number="CR25 1551 4467 2549 4402 1",
-#     storage_instructions="instrucao 1",
-# )
+produto3 = Product(
+    id="1",
+    product_name="Nicotine Polacrilex",
+    company_name="Target",
+    manufacturing_date="2021-02-18",
+    expiration_date="2023-08-27",
+    serial_number="CR25 1551 4467 2549 4402 1",
+    storage_instructions="instrucao 1",
+)
 
-# produto4 = Product(
-#     id="1",
-#     product_name="Nicotine Polacrilex",
-#     company_name="Corporation",
-#     manufacturing_date="2019-02-18",
-#     expiration_date="2023-08-26",
-#     serial_number="CR25 1551 4467 2549 4402 1",
-#     storage_instructions="instrucao 1",
-# )
-
-# inventory = Inventory([produto])
-# inventory2 = Inventory([produto3])
-# inventory3 = Inventory([produto4])
-# inventory.add_data([produto2])
-# teste = SimpleReport()
-# teste.add_inventory(inventory)
-# teste.add_inventory(inventory2)
-# teste.add_inventory(inventory3)
-# print(teste.generate())
+produto4 = Product(
+    id="1",
+    product_name="Nicotine Polacrilex",
+    company_name="Corporation",
+    manufacturing_date="2019-02-18",
+    expiration_date="2023-08-26",
+    serial_number="CR25 1551 4467 2549 4402 1",
+    storage_instructions="instrucao 1",
+)
+inventory4 = Inventory()
+inventory = Inventory([produto])
+inventory2 = Inventory([produto3])
+inventory3 = Inventory([produto4])
+inventory.add_data([produto2])
+teste = SimpleReport()
+teste.add_inventory(inventory)
+teste.add_inventory(inventory2)
+teste.add_inventory(inventory3)
+teste.add_inventory(inventory4)
+print(teste.generate())
--- inventory_report/reports/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-teste = datetime.strptime('2023-08-24', "%Y-%m-%d").date()
-teste2 = datetime.now().date()
-print(teste2 - teste)
