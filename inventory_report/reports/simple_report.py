928b6d9587098690776cedb5e42282b7f4b53e87
Pietro Bucker <pietro_177@hotmail.com>
2023-08-25 21:25:33 -0300

    requisito 7 testando

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -54,52 +54,52 @@ class SimpleReport(Report):
         )
 
 
-produto = Product(
-    id="1",
-    product_name="Nicotine Polacrilex",
-    company_name="Target Corporation",
-    manufacturing_date="2020-02-18",
-    expiration_date="2023-09-26",
-    serial_number="CR25 1551 4467 2549 4402 1",
-    storage_instructions="instrucao 1",
-)
+# produto = Product(
+#     id="1",
+#     product_name="Nicotine Polacrilex",
+#     company_name="Target Corporation",
+#     manufacturing_date="2020-02-18",
+#     expiration_date="2023-09-26",
+#     serial_number="CR25 1551 4467 2549 4402 1",
+#     storage_instructions="instrucao 1",
+# )
 
-produto2 = Product(
-    id="1",
-    product_name="Nicotine Polacrilex",
-    company_name="Corporation",
-    manufacturing_date="2022-01-18",
-    expiration_date="2023-09-17",
-    serial_number="CR25 1551 4467 2549 4402 1",
-    storage_instructions="instrucao 1",
-)
+# produto2 = Product(
+#     id="1",
+#     product_name="Nicotine Polacrilex",
+#     company_name="Corporation",
+#     manufacturing_date="2022-01-18",
+#     expiration_date="2023-09-17",
+#     serial_number="CR25 1551 4467 2549 4402 1",
+#     storage_instructions="instrucao 1",
+# )
 
-produto3 = Product(
-    id="1",
-    product_name="Nicotine Polacrilex",
-    company_name="Target",
-    manufacturing_date="2021-02-18",
-    expiration_date="2023-08-27",
-    serial_number="CR25 1551 4467 2549 4402 1",
-    storage_instructions="instrucao 1",
-)
+# produto3 = Product(
+#     id="1",
+#     product_name="Nicotine Polacrilex",
+#     company_name="Target",
+#     manufacturing_date="2021-02-18",
+#     expiration_date="2023-08-27",
+#     serial_number="CR25 1551 4467 2549 4402 1",
+#     storage_instructions="instrucao 1",
+# )
 
-produto4 = Product(
-    id="1",
-    product_name="Nicotine Polacrilex",
-    company_name="Corporation",
-    manufacturing_date="2019-02-18",
-    expiration_date="2023-08-26",
-    serial_number="CR25 1551 4467 2549 4402 1",
-    storage_instructions="instrucao 1",
-)
+# produto4 = Product(
+#     id="1",
+#     product_name="Nicotine Polacrilex",
+#     company_name="Corporation",
+#     manufacturing_date="2019-02-18",
+#     expiration_date="2023-08-26",
+#     serial_number="CR25 1551 4467 2549 4402 1",
+#     storage_instructions="instrucao 1",
+# )
 
-inventory = Inventory([produto])
-inventory2 = Inventory([produto3])
-inventory3 = Inventory([produto4])
-inventory.add_data([produto2])
-teste = SimpleReport()
-teste.add_inventory(inventory)
-teste.add_inventory(inventory2)
-teste.add_inventory(inventory3)
-print(teste.generate())
+# inventory = Inventory([produto])
+# inventory2 = Inventory([produto3])
+# inventory3 = Inventory([produto4])
+# inventory.add_data([produto2])
+# teste = SimpleReport()
+# teste.add_inventory(inventory)
+# teste.add_inventory(inventory2)
+# teste.add_inventory(inventory3)
+# print(teste.generate())
